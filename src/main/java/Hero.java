d7b1e4ee7e8d6f9ba4eccc0431a3e80124e7db8e
Megan Fayer, Mike Gallegos and Ryan King <student@epicodus.com>
2016-03-07 10:20:40 -0800

    begin building hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -8,15 +8,63 @@ import java.util.ArrayList;
 import org.sql2o.*;
 
 public class Hero {
+  private int id;
   private String name;
-  private int bitCoins;
+  private int gold;
   private int attack;
   private int defense;
   private int speed;
-  private int euphoria;
+  private int stamina;
+  private final static int MIN_STAMINA = 0;
+  private final static int MIN_ATTACK = 0;
+  private final static int MIN_DEFENSE = 0;
+  private final static int MIN_SPEED = 0;
+  private final static int MIN_GOLD = 0;
+  private final static int MAX_STAMINA = 50;
+  private final static int MAX_ATTACK = 15;
+  private final static int MAX_DEFENSE = 15;
+  private final static int MAX_SPEED = 15;
+  private final static int MAX_GOLD = 15;
 
 
   public Hero(String name) {
+    this.name = name;
+  }
 
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getCurrency() {
+    return gold;
+  }
+
+  public int getAttack() {
+    return attack;
+  }
+
+  public int getDefense() {
+    return defense;
+  }
+
+  public int getSpeed() {
+    return speed;
+  }
+
+  public int getStamina() {
+    return stamina;
+  }
+
+  public Boolean isAlive() {
+    int stamina = getStamina();
+    boolean isAlive = true;
+    if (stamina < 1) {
+      isAlive = false;
+    }
+    return isAlive
   }
 }
