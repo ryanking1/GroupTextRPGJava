58bfdcba73f8d54739ec4ed48eccc1d653b099ee
Megan <mefayer@gmail.com>
2016-03-10 11:19:37 -0800

    comment out the java pages we don't need

--- src/main/java/Armor.java
+++ src/main/java/Armor.java
@@ -1,35 +1,35 @@
-import java.util.List;
-import java.util.ArrayList;
-import org.sql2o.*;
-
-public class Armor {
-  private String armorName;
-  private int armorId;
-  private int armorDefense;
-
-
-  public String getArmorName() {
-    return armorName;
-  }
-
-  public int getArmorId() {
-    return armorId;
-  }
-
-  public int getArmorDefense() {
-    return armorDefense;
-  }
-
-  public void update(int id) {
-    this.armorName = Armor.getArmorName();
-    this.armorDefense = Armor.getArmorDefense();
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "UPDATE hero_armor SET armorName = :armorName, armorDefense = :armorDefense WHERE id = :id";
-      con.createQuery(sql)
-        .addParameter("armorName", armorName)
-        .addParameter("armorDefense", armorDefense)
-        .addParameter("id", id)
-        .executeUpdate();
-    }
-  }
-}
+// import java.util.List;
+// import java.util.ArrayList;
+// import org.sql2o.*;
+//
+// public class Armor {
+//   private String armorName;
+//   private int armorId;
+//   private int armorDefense;
+//
+//
+//   public String getArmorName() {
+//     return armorName;
+//   }
+//
+//   public int getArmorId() {
+//     return armorId;
+//   }
+//
+//   public int getArmorDefense() {
+//     return armorDefense;
+//   }
+//
+//   public void update(int id) {
+//     this.armorName = Armor.getArmorName();
+//     this.armorDefense = Armor.getArmorDefense();
+//     try(Connection con = DB.sql2o.open()) {
+//       String sql = "UPDATE hero_armor SET armorName = :armorName, armorDefense = :armorDefense WHERE id = :id";
+//       con.createQuery(sql)
+//         .addParameter("armorName", armorName)
+//         .addParameter("armorDefense", armorDefense)
+//         .addParameter("id", id)
+//         .executeUpdate();
+//     }
+//   }
+// }
--- src/main/java/Weapon.java
+++ src/main/java/Weapon.java
@@ -1,34 +1,34 @@
-import java.util.List;
-import java.util.ArrayList;
-import org.sql2o.*;
-
-public class Weapon {
-  private String wepName;
-  private int wepId;
-  private int wepDamage;
-
-  public String getWeaponName() {
-    return wepName;
-  }
-
-  public int getWeaponId() {
-    return wepId;
-  }
-
-  public int getWeaponDamage() {
-    return wepDamage;
-  }
-
-  public void update(int id) {
-    this.wepName = Weapon.getWeaponName();
-    this.wepDamage = Weapon.getWeaponDamage();
-    try(Connection con = DB.sql2o.open()) {
-      String sql = "UPDATE hero_weapon SET wepName = :wepName, wepDamage = :wepDamage WHERE id = :id";
-      con.createQuery(sql)
-        .addParameter("wepName", wepName)
-        .addParameter("wepDamage", wepDamage)
-        .addParameter("id", id)
-        .executeUpdate();
-    }
-  }
-}
+// import java.util.List;
+// import java.util.ArrayList;
+// import org.sql2o.*;
+//
+// public class Weapon {
+//   private String wepName;
+//   private int wepId;
+//   private int wepDamage;
+//
+//   public String getWeaponName() {
+//     return wepName;
+//   }
+//
+//   public int getWeaponId() {
+//     return wepId;
+//   }
+//
+//   public int getWeaponDamage() {
+//     return wepDamage;
+//   }
+//
+//   public void update(int id) {
+//     this.wepName = Weapon.getWeaponName();
+//     this.wepDamage = Weapon.getWeaponDamage();
+//     try(Connection con = DB.sql2o.open()) {
+//       String sql = "UPDATE hero_weapon SET wepName = :wepName, wepDamage = :wepDamage WHERE id = :id";
+//       con.createQuery(sql)
+//         .addParameter("wepName", wepName)
+//         .addParameter("wepDamage", wepDamage)
+//         .addParameter("id", id)
+//         .executeUpdate();
+//     }
+//   }
+// }
